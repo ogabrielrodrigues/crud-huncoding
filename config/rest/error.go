b364bf87d0a3aa6e3801a7ad56b543e81b783eeb
ogabrielrodrigues <gabrielrodrigues409@outlook.com>
2023-12-01 19:40:19 -0300

    project structure and rest package

---
+++ config/rest/error.go
@@ -0,0 +1,64 @@
+package rest
+
+import "net/http"
+
+type RestErr struct {
+	Message string  `json:"message"`
+	Err     string  `json:"error"`
+	Code    int     `json:"code"`
+	Causes  []Cause `json:"causes,omitempty"`
+}
+
+type Cause struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+func NewRestErr(message, err string, code int, causes []Cause) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestErr(message string, causes []Cause) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+		Causes:  nil,
+	}
+}
+
+func NewNotFoundErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+		Causes:  nil,
+	}
+}
+
+func NewForbiddenErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+		Causes:  nil,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ogabrielrodrigues/crud-huncoding
+
+go 1.21.0
